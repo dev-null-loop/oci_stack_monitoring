cf18ad2bf41387c1a84fc13e855340f858510ae9
dev-null-loop <dev.null.loop@gmail.com>
2026-05-18 13:40:37 +0300

    Add Stack Monitoring maintenance window retry-failed-operation module

---
+++ maintenance_windows_retry_failed_operation/main.tf
@@ -0,0 +1,3 @@
+resource "oci_stack_monitoring_maintenance_windows_retry_failed_operation" "this" {
+  maintenance_window_id = var.maintenance_window_id
+}
---
+++ maintenance_windows_retry_failed_operation/outputs.tf
@@ -0,0 +1,4 @@
+output "id" {
+  description = "The ID of the resource."
+  value       = oci_stack_monitoring_maintenance_windows_retry_failed_operation.this.id
+}
